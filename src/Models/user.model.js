170e11ad37743c2306b634ecfaf1a64cbb39b24f
nahid-105-ontor <ontornahidhasan65@gmail.com>
2026-06-14 14:27:49 +0600

    bcrypt and jwt sample

---
+++ src/Models/user.model.js
@@ -0,0 +1,104 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true, //cloudnary service
+    },
+    coverImage: {
+      type: String,
+      required: true, //cloudnary service
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+      trim: true,
+      unique: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+})
+
+
+userSchema.methods.isPasswordCorrect = async function(password){
+ return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      espiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+}
+
+
+
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      espiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+}
+
+
+
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
