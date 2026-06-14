b3c5a9dd366454845df5dff12c91c54dc5adc0fb
nahid-105-ontor <ontornahidhasan65@gmail.com>
2026-06-14 18:48:51 +0600

    multer middle ware with cloudinary

---
+++ src/Utilities/fileUpload.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) {
+      return null;
+    }
+    const response = await cloudinary.uploader.upload(filePath,{
+        resource_type:"auto"
+    })
+    console.log("File Successfully Uploaded On Cloudinary",response.url);
+    return response
+  } catch(error) {
+    fs.unlinkSync(filePath)
+    return null;
+  }
+};
+
+module.exports = cloudinary;
